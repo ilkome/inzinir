38d14a3686ced7f0df6a59547cd67bc8322555c1
style-nes <style-nes@ya.ru>
2014-11-14 16:27:03 +0300

    Автоматическое листание слайдера

--- site/js/functions.js
+++ site/js/functions.js
@@ -197,6 +197,8 @@ $(document).ready(function() {
 	if ( $.isFunction($.fn.lightSlider) ) {
 		$(".slider-index").lightSlider({
 			item: 1,
+			loop: true,
+			auto: true,
 			cssEasing: 'cubic-bezier(0.25, 0, 0.25, 1)',
 				swipeThreshold: 100,
 		});
